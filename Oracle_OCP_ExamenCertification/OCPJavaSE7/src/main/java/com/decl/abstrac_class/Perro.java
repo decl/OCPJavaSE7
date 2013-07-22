8f96bd4fb6103bb3df487535d16c49529ebee95c
decl <decl_151@hotmail.com>
2013-07-22 13:02:00 -0500

    Agregue clase perro

---
+++ Oracle_OCP_ExamenCertification/OCPJavaSE7/src/main/java/com/decl/abstrac_class/Perro.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.decl.abstrac_class;
+
+/**
+ *
+ * @author DSA-David
+ */
+public class Perro extends Animal{
+    
+}

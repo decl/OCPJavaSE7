c016d52fb79177e92d465f339441361c85f3a6cf
decl <decl_151@hotmail.com>
2013-09-10 11:54:16 -0500

    Agregue clase collections

---
+++ Oracle_OCP_ExamenCertification/OCPJavaSE7/src/main/java/com/decl/collections/Collections.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.decl.collections;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+/**
+ *
+ * @author DSA-David
+ */
+public class Collections extends Thread{
+    private HashSet<String> listSet;
+    
+    public void ejemploUno(){
+        
+    }
+
+}
